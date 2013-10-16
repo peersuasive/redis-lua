137a8c56cbc517c88102c252a4c173a98f03eee5
Christophe Berbizier <cberbizier@peersuasive.com>
2013-10-16 03:13:18 +0200

    refactored zsocket

--- src/zsocket.lua
+++ src/zsocket.lua
@@ -15,9 +15,17 @@ zsocket.lua
 
 local zmq = require "zmq"
 
+--[[ MODULE ]]------------------------------------------------------------------
+
 local meta = {}
 meta.__index = meta
 
+
+--[[ METHODS ]]-----------------------------------------------------------------
+
+--[[--
+connect to redis socket and set ID
+--]]
 local function connect(self, addr)
     -- ... connect
     local ok, err = self.socket:connect( addr )
@@ -26,51 +34,39 @@ local function connect(self, addr)
     return ok
 end
 
+--[[--
+send command to redis with socket id
+--]]
 local function send(self, data)
     self.socket:send( self.socket_id, zmq.SNDMORE )
     self.socket:send( string.format(data), zmq.SNDMORE )
 end
 
+--[[--
+read data and cache unused data, zmq won't keep them waiting the way a BSD socket would
+--]]
 local function receive(self, len, ...)
     if len == nil then len = '*l' end
     local line = nil
-    if (self.cache and #self.cache>0) then
-        if ("*l"==len) then
-            local pos, xpos = self.cache:find("\r?\n")
-            pos, xpos = pos or 0, xpos or 0
-            local margin = xpos - pos
-            line = self.cache:sub( 0, pos-1 )
-            self.cache = self.cache:sub(pos+1+margin)
-        elseif ("*a"==len) then
-            line = self.cache
-            self.cache = ""
-        elseif ("number"==type(len)) then
-            line = self.cache:sub(0, len)
-            self.cache = self.cache:sub( len+1 )
-        else
-            print("Can't understand len type: " .. tostring(len))
-            error("Can't understand len type: " .. tostring(len))
-        end
-    else
-        -- read fresh data
+    if not (self.cache and #self.cache>0) then
         -- discard ID
         self.socket:recv()
-        local xline
-        xline, err = self.socket:recv()
+        self.cache, err = self.socket:recv()
+    end
+    if ("*l"==len) then
+        local pos, xpos = self.cache:find("\r?\n")
+        pos, xpos = pos or 0, xpos or 0
+        local margin = xpos - pos
+        line = self.cache:sub( 0, pos-1 )
+        self.cache = self.cache:sub(pos+1+margin)
+    elseif ("*a"==len) then
+        line = self.cache
         self.cache = ""
-        if ("*l"==len) then
-            local pos, xpos = xline:find("\r?\n")
-            pos, xpos = pos or 0, xpos or 0
-            local margin = xpos - pos
-            line = xline:sub(0, pos - 1 )
-            self.cache = xline:sub( pos+1+margin )
-        elseif("*a"==len) then
-            line = line
-            self.cache = ""
-        else
-            print("Unknown len case: "..tostring(len))
-            error("Unknown len case: " .. tostring(len))
-        end
+    elseif ("number"==type(len)) then
+        line = self.cache:sub(0, len)
+        self.cache = self.cache:sub( len+1 )
+    else
+        error("Can't understand len type: " .. tostring(len))
     end
 
     if not err then 
@@ -80,6 +76,9 @@ local function receive(self, len, ...)
     end
 end
 
+--[[--
+close connection, terminate context
+--]]
 local function close(self, ...)
     self.socket:setopt(zmq.LINGER, 0)
     self.socket:close()
@@ -87,6 +86,10 @@ local function close(self, ...)
     self.cache = nil
 end
 
+
+--[[--
+init socket
+--]]
 local function __call(self, ...)
     local self = {}
     self.connect    = connect
